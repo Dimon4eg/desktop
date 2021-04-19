84ec5b0b21351981aa2304afedc8a6f86184ff2f
Felix Weilbach <felix.weilbach@nextcloud.com>
2021-04-19 16:08:00 +0200

    Add CMake option to enable address sanitizers
    Signed-off-by: Felix Weilbach <felix.weilbach@nextcloud.com>

--- cmake/modules/SanitizerFlags.cmake
+++ cmake/modules/SanitizerFlags.cmake
@@ -1,37 +1,47 @@
-# Enable address sanitizer (gcc/clang only)
-if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-    set(SANITIZERS)
-    set(SANITIZER_EXTRA_FLAGS " -g")
+# Keep in mind that some combinations will not work together
+option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer to detect memory violations, buffer overflows, memory leaks" OFF)
+option(ENABLE_SANITIZER_LEAK "Enable leak sanitizer to detect memory leaks" OFF)
+option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer to detect reads in unitialized memory" OFF)
+option(ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer to detect undefined behavior" OFF)
+option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer to detect data races" OFF)
 
-    macro(add_sanitizer_option variable flag help)
-        option(${variable} "Enable ${help}" OFF)
-        if(${variable})
-            list(APPEND SANITIZERS ${flag})
-            string(REPLACE ";" " " optional_args "${ARGN}")
-            if(optional_args)
-              string(APPEND SANITIZER_EXTRA_FLAGS " ${optional_args}")
-            endif()
-        endif()
-        mark_as_advanced(${variable})
-    endmacro()
+set(ENABLED_SANITIZERS)
+mark_as_advanced(ENABLED_SANITIZERS)
 
-    add_sanitizer_option(SANITIZE_ADDRESS "address"
-        "AddressSanitizer (detects memory violations, buffer overflows, memory leaks)")
-    add_sanitizer_option(SANITIZE_LEAK "leak"
-        "standalone LeakSanitizer (detects memory leaks only)")
-    add_sanitizer_option(SANITIZE_MEMORY "memory"
-        "MemorySanitizer (detects reads in uninitialized memory)")
-    add_sanitizer_option(SANITIZE_UNDEFINED "undefined"
-        "UndefinedBehaviorSanitizer (detects undefined behavior)"
-        "-fno-sanitize=vptr")
-    add_sanitizer_option(SANITIZE_THREAD "thread"
-        "ThreadSanitizer (detects data races)")
+macro(add_sanitizer_option variable flag)
+  if(${variable})
+    list(APPEND ENABLED_SANITIZERS ${flag})
+  endif()
+endmacro()
 
-    if(SANITIZERS)
-        string(REPLACE ";" "," SANITIZER_FLAGS "${SANITIZERS}")
-        set(SANITIZER_FLAGS "-fsanitize=${SANITIZER_FLAGS}${SANITIZER_EXTRA_FLAGS}")
-        string(APPEND CMAKE_CXX_FLAGS " ${SANITIZER_FLAGS}")
-        string(APPEND CMAKE_C_FLAGS " ${SANITIZER_FLAGS}")
-        string(APPEND CMAKE_EXE_LINKER_FLAGS " ${SANITIZER_FLAGS}")
-    endif()
-endif()
+add_sanitizer_option(ENABLE_SANITIZER_ADDRESS "address")
+add_sanitizer_option(ENABLE_SANITIZER_LEAK "leak")
+add_sanitizer_option(ENABLE_SANITIZER_MEMORY "memory")
+add_sanitizer_option(ENABLE_SANITIZER_UNDEFINED "undefined")
+add_sanitizer_option(ENABLE_SANITIZER_THREAD "thread")
+
+function(enable_sanitizers target)
+  if(ENABLED_SANITIZERS)
+    string(REPLACE ";" "," enabled_sanitizer_flags "${ENABLED_SANITIZERS}")
+    message(STATUS "Enabled ${enabled_sanitizer_flags} sanitizers on ${target}")
+
+
+    target_compile_options(${target} PRIVATE
+      $<$<CXX_COMPILER_ID:MSVC>:-fsanitize=${enabled_sanitizer_flags}>
+      $<$<CXX_COMPILER_ID:Clang>:-g>
+      $<$<CXX_COMPILER_ID:GNU>:-g>
+      )
+
+    target_link_libraries(${target} PRIVATE
+      $<$<CXX_COMPILER_ID:Clang>:-fsanitize=${enabled_sanitizer_flags}>
+      $<$<CXX_COMPILER_ID:GNU>:-fsanitize=${enabled_sanitizer_flags}>
+      )
+
+    target_link_options(${target} PRIVATE
+      # Until version 16.9 Preview 2 of Visual Studio, we need to link manually against the asan libs
+      # https://devblogs.microsoft.com/cppblog/addresssanitizer-asan-for-windows-with-msvc/#compiling-with-asan-from-the-console
+      $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<VERSION_LESS_EQUAL:$ENV{VSCMD_VER},16.9.2>>:/wholearchive:clang_rt.asan_dynamic_runtime_thunk-x86_64.lib /wholearchive:clang_rt.asan_dynamic-x86_64.lib>
+      )
+
+  endif()
+endfunction()
